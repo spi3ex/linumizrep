b328f7288baf6d0f3f44b67909260e47fce92c98
Jackson R <48877885+spi3ex@users.noreply.github.com>
2020-10-04 19:05:07 +0530

    Pointer dereferencing and C99 Conflicts;
    SOLVED:
    #1. c99 Standard doesn't allow the use of Certain Function Names
    #2. Pointer Conflicts by dereferencing

--- linkedlistreverse.c
+++ linkedlistreverse.c
@@ -8,60 +8,62 @@ struct node{
 }; //initialized node
 typedef struct node *Node; // used typedef to define a more readable coee
 
+void Linklist(Node *ref,int input);
+void LinkedReverse(Node head);
 int main(int argc,char *argv[]) //get arguments from the commandline
 {
-    char *text;
-    text=argv[1];
-    Node* head = NULL;
+	Node head;
+	head = NULL; //Creating head node
+	head = (Node)malloc(sizeof(struct node));
+    char *text; //text stores the string
+    text=argv[1]; //Commandline arguments passed to text
 
     for (int i=0;i<strlen(text);i++)
     {
-        CreateNode(text[i]);
+        Linklist(&head,text[i]); //Creating node for each string
     }
-    ReverseNode(head);
+    LinkedReverse(head);
     return 0;
 }
-
-
-void CreateNode(int input)
+void Linklist(Node *ref,int input)
 {
-    Node *NewNode;
-    Node *temp;
-    NewNode= (Node*) malloc(sizeof(struct node));
+    Node NewNode;
+    Node temp;
+    NewNode= (Node) malloc(sizeof(struct node));
 
-   
     NewNode->data = input;
     NewNode->next = NULL;
 
-    if (*head == NULL)
+    if (ref == NULL)
     {
-       *head = NewNode;
+       *ref = NewNode;
        return;
     }
-    temp = head;
+    temp = *ref;
     while (temp->next != NULL){
         temp = temp->next;
     }
     temp->next = NewNode;
+    NewNode->next = NULL;
     return;
 }
 
 
 
-void ReverseNode(head)
+void LinkedReverse(Node head)
 {
-    Node* prev = NULL;
-    Node* current = *head;
-    Node* next = NULL;
+    Node prev = NULL;
+    Node current = head;
+    Node next = NULL;
     while (current != NULL) {
         next = current->next;
         current->next = prev;
         prev = current;
         current = next;
     }
-    *head = prev;
-    Node *temp;
-    head = temp;
+    head = prev;
+    Node temp;
+    temp = head;
     while(temp->next != NULL){
 		printf("%c",temp->data);
 		temp = temp->next;
